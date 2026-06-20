a87309e9b0b0d98fdc424abb482edc80b5fc1fba
VictorAgahi <victor@agahi.com>
2026-06-20 16:24:25 +0200

    chore(all): add CI/CD test log to other post-processors

--- post-processor-social/src/index.ts
+++ post-processor-social/src/index.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const config = loadConfig(configDir, CustomConfig);
   const port = config.port;
 
-  logger.info('Bootstrapping NestJS application...');
+  logger.info('Bootstrapping NestJS application... (CI/CD test)');
 
   const app = await NestFactory.create(AppModule.register(config), {
     logger,
